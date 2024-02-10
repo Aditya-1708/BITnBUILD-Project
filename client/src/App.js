aa12cbcb985aa920e710c4d33a918a6b01a1005f
iamabhishekagarwal <iamabhishekagarwal1999@gmail.com>
2024-02-10 15:51:18 +0530

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Email from "./pages/Email";
 import MyCalendar from "./pages/MyCalendar";
+import Otp from "./pages/Otp";
+import Signin from "./pages/Signin";
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      {/* <Signin></Signin> */}
-      {/* <Email></Email> */}
-      {/* <Otp></Otp> */}
-      {/* <MyCalendar></MyCalendar> */}
+      <Router>
+        <Routes>
+        <Route  ></Route>
+          <Signin></Signin>
+          <Email></Email>
+          <Otp></Otp>
+          <Navbar></Navbar>
+          <MyCalendar></MyCalendar>
+        </Routes>
+      </Router>
     </>
-    
   );
 }
 
