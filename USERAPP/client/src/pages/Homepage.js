d2c6f4d4c566c517f2005191b9610de310016541
iamabhishekagarwal <iamabhishekagarwal1999@gmail.com>
2024-02-11 05:56:31 +0530

    Added pics

--- USERAPP/client/src/pages/Homepage.js
+++ USERAPP/client/src/pages/Homepage.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
 import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import second from "../img/1.jpg";
+import three from "../img/10.jpg";
+import third from "../img/2.png";
+import fourth from "../img/3.png";
+import fifth from "../img/4.png";
+import sixth from "../img/5.jpg";
+import one from "../img/6.png";
+import eight from "../img/7.png";
+import two from "../img/8.png";
+import seventh from "../img/9.jpg";
+
 import "./Homepage.css";
 
 import { Pagination } from "swiper/modules";
@@ -18,24 +28,22 @@ return (
         modules={[Pagination]}
         className="mySwiper"
     >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide><img style={{width:"auto",height:"100%"}} src="https://static.toiimg.com/thumb/msid-103068370,width-1280,height-720,resizemode-4/103068370.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img style={{width:"auto", height:"100%"}} src={second} alt="" /></SwiperSlide>
+        <SwiperSlide><img style={{width:"auto", height:"100%"}} src={third} alt="" /></SwiperSlide>
+        <SwiperSlide><img style={{width:"auto", height:"100%"}} src={fourth} alt="" /></SwiperSlide>
+        <SwiperSlide><img style={{width:"auto", height:"100%"}} src={fifth} alt="" /></SwiperSlide>
+        <SwiperSlide><img style={{width:"auto", height:"100%"}} src={sixth} alt="" /></SwiperSlide>
+        <SwiperSlide><img style={{width:"auto", height:"100%"}} src={seventh} alt="" /></SwiperSlide>
+        <SwiperSlide><img style={{width:"auto", height:"100%"}} src={eight} alt="" /></SwiperSlide>
     </Swiper>
 
     <h1 id="SuggestedContent" style={{marginTop:"80px"}}>Suggested Content</h1>
     <div  className="image-grid">
-        <img  src="image1.jpg" alt="Image 1" />
-        <img src="image2.jpg" alt="Image 2" />
+        <img  src={one} alt="Image 1" />
+        <img src={two} alt="Image 2" />
+        <img src={three} alt="Image 3" />
         <img src="image3.jpg" alt="Image 3" />
-        <img src="image3.jpg" alt="Image 3" />
-        <img src="image3.jpg" alt="Image 4" />
     </div>
     </div>
 </>
